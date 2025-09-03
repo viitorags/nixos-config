3a1e1df2241ea49fbc47fd51411111f9b5407a03
viitorags <vitorgabriels1000@gmail.com>
2025-09-03 14:54:30 -0300

    feat(home): remove Hyprland configuration
    Removes all Hyprland configuration files and auxiliary scripts:
    - Hyprland main configuration files
    - Hyprlock configuration
    - Scripts for clipboard management, application launcher, power menu,
    screenshot, and wallpaper changer

--- home/modules/hyprland/scripts/clipboard.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-wallpaper_path=$(swww query | sed -n 's/.*currently displaying: image: \(.*\)/\1/p')
-
-selected=$(
-    cliphist list -t |
-        rofi -modi clipboard:~/nixos-config/home/modules/hyprland/scripts/cliphist-rofi-img -show clipboard -show-icons -font "Fantasque Sans Mono Regular 15" -theme ~/.config/rofi/launcher.rasi
-    # -theme-str "inputbar { background-image: url(\"$wallpaper_path\", height); }"
-)
--- home/modules/hyprland/scripts/launcher.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-wallpaper_path=$(swww query | sed -n 's/.*currently displaying: image: \(.*\)/\1/p')
-rofi -show drun -font "Sarasa Gothic CL 12" \
-    -theme ~/.config/rofi/launcher.rasi
-# -theme-str "imagebox { background-image: url(\"$wallpaper_path\", height);}"
